aca134c2c2c57fe49effe4944a9827d4fd3048bc
matheusroccogg <matheusroccogg@gmail.com>
2025-11-28 22:59:20 +0000

    start project

---
+++ cardapio.py
@@ -0,0 +1,16 @@
+def get_cardapio():
+    cardapio = [
+    {
+        'id': '1',
+        'titulo': 'bolo de chocolate',
+        'descricao': 'Delicioso bolo de morango',
+        'foto': ''
+    },
+    {
+        'id': '2',
+        'titulo': 'bolo de morango',
+        'descricao': 'Delicioso bolo de morango',
+        'foto': ''
+    }
+    ]
+    return cardapio
